f9db41d4fa9fc55e9008c32e75719a9f21c1f328
Auth <111557161+Authtbh@users.noreply.github.com>
2024-02-22 21:15:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,36 @@ import './index.css';
 function App() {
   const [value, setValue] = useState('');
   const [result, SetResult] = useState('');
+  const [hint, setHint] = useState('');
+  const [tries, setTries] = useState(0);
 
   const check = (e) => {
-    const getRandomInt = Math.floor(Math.random() * 10);
+    const getRandomInt = Math.floor(Math.random() * 100);
     console.log(getRandomInt);
     e.preventDefault();
-    const inputValue = parseInt(value, 10); // Convert input value to a number
+    setTries(tries + 1);
+    
+    const inputValue = parseInt(value); // Convert input value to a number
     if (inputValue === getRandomInt) {
       console.log("Yeah, you did it!");
-      SetResult("Answer is correct");
-    } else {
-      console.log("Nuh uh");
+      SetResult(`Hurray! you guessed the number in ${tries} tries`);
+      setTries(0);
+      setHint("");
+    } else if (inputValue > getRandomInt) {
+      console.log("Nuh uh ");
       SetResult("Answer is not correct");
+      setHint("Too big");
+    } else if (inputValue < getRandomInt) {
+      console.log("Nuh uh ");
+      SetResult("Answer is not correct");
+      setHint("Too Small");
+    } else {
+      console.log("Nuh uh ");
+      SetResult("Answer is not correct");
+      setHint("Tf you did");
     }
   };
+  
 
   return (
     <>
@@ -27,7 +43,8 @@ function App() {
       <h3 className={`text-2xl flex justify-center items-center ${result === "Answer is not correct" ? 'text-red-500' : 'text-lime-400'}`}>
         {result}
       </h3>
-
+      <h5 className={`flex justify-center items-center ${hint == "Too big" ? 'text-red-700' :  'text-blue-600'}`}>{hint}</h5>
+      <h6 className="flex items-center justify-center text-xl text-stone-100 mt-10 -mb-16">Total Tries:{tries}</h6>
       <form className="flex justify-center items-center mt-24" onSubmit={check}>
         <label>
           <input
