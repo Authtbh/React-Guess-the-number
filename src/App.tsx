e46e1ca1e6f0f53f22de9a013cdb2e9420c6edfa
Auth <111557161+Authtbh@users.noreply.github.com>
2024-02-22 18:26:53 +0530

    fixed lil errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ function App() {
   const [value, setValue] = useState('');
   const [result, SetResult] = useState('');
 
-  
-
-  const check = (e) => {
+  const check = (e: React.FormEvent<HTMLFormElement>) => {
     const getRandomInt = Math.floor(Math.random() * 10);
     console.log(getRandomInt);
     e.preventDefault();
@@ -15,7 +13,6 @@ function App() {
     if (inputValue === getRandomInt) {
       console.log("Yeah, you did it!");
       SetResult("Answer is correct");
-
     } else {
       console.log("Nuh uh");
       SetResult("Answer is not correct");
@@ -28,8 +25,8 @@ function App() {
         <h1>Guess the Number</h1>
       </div>
       <h3 className={`text-2xl flex justify-center items-center ${result === "Answer is not correct" ? 'text-red-500' : 'text-lime-400'}`}>
-  {result}
-</h3>
+        {result}
+      </h3>
 
       <form className="flex justify-center items-center mt-24" onSubmit={check}>
         <label>
@@ -42,7 +39,9 @@ function App() {
           />
         </label>
         <label>
-        <button type = 'submit' class="rounded-md bg-slate-300 ml-5">Click to submit</button>
+          <button type="submit" className="rounded-md bg-slate-300 ml-5">
+            Click to submit
+          </button>
         </label>
       </form>
     </>
